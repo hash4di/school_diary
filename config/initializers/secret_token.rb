fd13260fc1d177803f82a083fa7a09befdfd548e
hash4di <hash4di@doubledrones.com>
2011-06-17 10:39:19 +0200

    add initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolDiary::Application.config.secret_token = '90e236bf919dc9d0238134f08c5acf3859a3ff2f10d335a701131cbd56c064d9b193f10b2f0864099ace1f0fd73d8e674f1e8f8ef33f87ca7bdf59a8aa39f2d6'
